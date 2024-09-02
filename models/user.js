3531d64803175211c9b4c9d511dc80268c995bed
Kim Seok Jin <kimseokjin6519@gmail.com>
2024-09-02 04:47:30 +0000

    profile api

--- models/user.js
+++ models/user.js
@@ -3,20 +3,26 @@ const Schema = mongoose.Schema;
 
 // Define the User schema
 const userSchema = new Schema({
-  email: {
+ email: {
     type: String,
     required: true,
     unique: true, // Ensures email is unique across the collection
     trim: true // Removes leading and trailing whitespace
   },
+  name: {
+    type: String,
+    required: true // Ensures name is also required
+  },
   password: {
     type: String,
     required: true
+  },
+  profile_picture: {
+    type: String // Optional, add if you're storing a URL or base64 string
   }
 }, {
   collection: 'users' // Explicitly specify the collection name
 });
-
 // Create the User model
 const User = mongoose.model('User', userSchema);
 
