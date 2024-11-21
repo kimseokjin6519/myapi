54d4f3214502953637c690d62b9b99079aad97bf
Kim Seok Jin <kimseokjin6519@gmail.com>
2024-11-21 08:22:12 +0000

    trending, gemini ai, keywords

--- routes/trending.js
+++ routes/trending.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Video = require('../models/videos');
 
 router.get('/', async (req, res) => {
 
-   const url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&regionCode=US&key=AIzaSyC0m-M0VsJPThT8MduBS-uFzDXeduPyBfk&maxResults=25';
-
    try {
-      const response = await fetch(url);
-      const data = await response.json();
 
-      const extract = data.items.map(item => ({
+      const myVideos = await Video.find({});
+      const myVideosList = myVideos.map(video => {
+         return `https://www.youtube.com/watch?v=${video.videoID}`;
+      });
+
+      const url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&regionCode=US&key=AIzaSyC0m-M0VsJPThT8MduBS-uFzDXeduPyBfk&maxResults=25';
+      const response = await fetch(url);
+      const youtubeMostPopular = await response.json();
+      const youtubeMostPopularList = youtubeMostPopular.items.map(item => ({
          title: item.snippet.title,
          description: item.snippet.description
       }));
 
-      const message = 'This is a list of YouTube videos. Please extrapolate data and return popular media content as keywords only.\n';
+      const message =   `Take no more than 5 seconds to return a result or return Busy. Please try again in a few moments. Analyze the first list of 
+                         YouTube videos (use title and description) to discover why they are popular, then return only a string of popular keywords
+                         separated by comma from analysis.
+                         `;
 
       const geminiUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyCU2ZuEIJRNuwYB719XCC8Pbvwlzvzsjbc';
       const geminiResponse = await fetch(geminiUrl, {
@@ -27,7 +35,7 @@ router.get('/', async (req, res) => {
                {
                   parts: [
                      {
-                        text: message + JSON.stringify(extract) // Sending extracted YouTube data as JSON
+                        text: message + JSON.stringify(youtubeMostPopularList) + JSON.stringify(myVideosList)// Sending extracted YouTube data as JSON
                      }
                   ]
                }
@@ -37,8 +45,8 @@ router.get('/', async (req, res) => {
 
       const geminiData = await geminiResponse.json();
 
-      // Send the Gemini response back to the client
-      res.json(geminiData);
+      res.send(geminiData);
+
    } catch (error) {
       console.error('Error fetching data:', error);
       res.status(500).send('Failed to fetch data');
